9207de5a2b827de0e86e1115d43c6a856dc9eaa2
riquerofranco32-prog <riquerofranco32@gmail.com>
2026-04-13 20:17:26 -0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -188,7 +188,7 @@ export function Hero() {
             fontFamily: "'Jura', sans-serif",
             fontWeight: 700,
             fontSize: "clamp(5rem, 20vw, 17rem)",
-            color: "#94f1be",
+            color: "#2C6E49",
           }}
         >
           SENTINEL
